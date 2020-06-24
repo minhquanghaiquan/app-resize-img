89da17bf8d24b316161edb4d8f264cbbf5af5af9
minhquanghaiquan <minhquanghaiquan@gmail.com>
2020-06-24 09:45:00 +0200

    change dir

--- routes/home.js
+++ routes/home.js
@@ -4,13 +4,14 @@ var multer  = require('multer');
 const sharp = require('sharp');
 const AdmZip = require('adm-zip');
 var fs = require('fs'); 
+var path = require('path')
 
 
 
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './public/uploads')
+      cb(null, './public/uploads') 
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
@@ -19,12 +20,17 @@ var storage = multer.diskStorage({
    
 var upload = multer({ storage: storage }).single('imageUpload');
 
-
+// '/'+req.file.path.split(path.sep).slice(1).join('/')
 function resize(data,size) {
-    return new Promise ((resolve, reject) => {
-		sharp(data.path)
+    const dir = __dirname.split(path.sep);
+    // console.log(array)
+    const index = dir.length ; 
+    const newDir = dir.slice(0,index-1).join('/') ; 
+    // console.log(newArray.join('/'));
+    return new Promise ((resolve, reject) =>{
+		sharp(newDir+'/'+data.path)
         .resize(size, size)
-        .toFile('public/uploads/thumb/thumb_'+size+'x'+size+'_'+data.originalname, function (err, info) {
+        .toFile(newDir+'/'+'public/uploads/thumb/thumb_'+size+'x'+size+'_'+data.originalname, function (err, info) {
             if(!err) {
                 console.log('sharp worked');
                 resolve();
@@ -49,7 +55,11 @@ router.post('/',upload ,  function(req, res) {
         resize(req.file , 128)
     ]).then(() => {
 
-        var uploadDir = fs.readdirSync("public/uploads/thumb/"); 
+        const dir = __dirname.split(path.sep);
+    // console.log(array)
+        const index = dir.length ; 
+        const newDir = dir.slice(0,index-1).join('/') ; 
+        var uploadDir = fs.readdirSync(newDir+"/public/uploads/thumb/"); 
         console.log(uploadDir);
 
         const newUploadDir=uploadDir.filter((item)=>{
@@ -60,23 +70,22 @@ router.post('/',upload ,  function(req, res) {
         const zip = new AdmZip();
 
         for(var i = 0; i < newUploadDir.length;i++){
-            zip.addLocalFile("public/uploads/thumb/"+newUploadDir[i]);
+            zip.addLocalFile(newDir+"/public/uploads/thumb/"+newUploadDir[i]);
         }
         //save file zip in local
         const downloadName = `${Date.now()}.zip`;
-        zip.writeZip('public/'+downloadName);
+        zip.writeZip(newDir+'/public/'+downloadName);
         //send file zip
         // const data = zip.toBuffer();
         // res.set('Content-Type','application/octet-stream');
         // res.set('Content-Disposition',`attachment; filename=${downloadName}`);
         // res.set('Content-Length',data.length);
         // res.send(data);
-        res.redirect('/public/'+downloadName);
+        res.redirect('public/'+downloadName);
     }).catch( (err) => {
         console.log(err);
     })
 })
-
 
 
 module.exports = router;
